ecaca453ecf4dddda313dcae40a61b4636151789
Marcel Cruz <marcelcruz@live.com>
2019-04-24 22:06:19 -0300

    Add CSS modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,60 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+// import 'components/header.module.scss'
+import headerStyles from 'components/header.module.scss'
+
 const Header = () => {
   return (
-    <nav>
-      <ul>
+    <header className={headerStyles.header}>
+      <h1>
+        <Link
+          className={headerStyles.title}
+          activeClassName={headerStyles.activeNavItem}
+          to="/"
+        >
+          Marcel Cruz
+        </Link>
+      </h1>
+      <ul className={headerStyles.navList}>
         <li>
-          <Link to="/">Home</Link>
+          <Link
+            className={headerStyles.navItem}
+            activeClassName={headerStyles.activeNavItem}
+            to="/"
+          >
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/blog">Blog</Link>
+          <Link
+            className={headerStyles.navItem}
+            activeClassName={headerStyles.activeNavItem}
+            to="/blog"
+          >
+            Blog
+          </Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link
+            className={headerStyles.navItem}
+            activeClassName={headerStyles.activeNavItem}
+            to="/about"
+          >
+            About
+          </Link>
         </li>
         <li>
-          <Link to="/contact">contact</Link>
+          <Link
+            className={headerStyles.navItem}
+            activeClassName={headerStyles.activeNavItem}
+            to="/contact"
+          >
+            Contact
+          </Link>
         </li>
       </ul>
-    </nav>
+    </header>
   )
 }
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Header from 'components/header'
 import Footer from 'components/footer'
 import 'styles/index.scss'
+import layoutStyles from 'components/layout.module.scss'
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <Header />
-      {children}
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
+        <Header />
+        {children}
+      </div>
+
       <Footer />
     </div>
   )

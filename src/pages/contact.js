1cd499011629d2fa62168cfaaa45369ac75c1eb9
Marcel Cruz <marcelcruz@live.com>
2019-04-24 21:07:27 -0300

    Add more pages and navigation

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>
+        You can reach me on <a href="https://twitter.com/marcelcruz">Twitter</a>
+        .
+      </p>
+    </div>
+  )
+}
+
+export default ContactPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <h2>I'm Marcel, a full-stack developer from Brazil.</h2>
+      <p>Need a developer?</p> <Link to="/contact">Contact me.</Link>
+    </div>
+  )
+}
+
+export default IndexPage
